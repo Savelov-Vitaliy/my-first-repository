ffe86f15b11a93e494c4c7667677b2fa545e3d58
Savelov-Vitaliy <savelov_vitaliy@mail.ru>
2018-03-20 17:00:34 +0500

    Lesson 1 - finish 2

--- Lesson_1/discriminant.rb
+++ Lesson_1/discriminant.rb
@@ -1,20 +1,16 @@
 #Решение квадратного уравнения
 
-print "Введите коэффицианты a, b, c для уравнения ax^2 + bx + c =0 через пробел: "
-abc = gets.chomp.split(' ')
-abc = abc.map{ |f| f.to_f }
 
-a = abc[0] 
-b = abc[1]
-c = abc[2] 
+print "Введите коэффицианты a, b, c для уравнения ax^2 + bx + c =0 через пробел: "
+a, b, c = gets.chomp.split(' ').map{ |f| f.to_f }
 
 d = b**2 - 4 * a * c
 
 if d >= 0
-	x_1 = ( -b + Math.sqrt(d) ) / (2*a)
-	x_2 = ( -b - Math.sqrt(d) ) / (2*a)
-	print "Дискриминант = #{d}. x_1 = #{x_1}"
-	puts "#{ d>0 ? ' , x_2 = '+x_2.to_s : ' ' }" 
+  x_1 = (-b + Math.sqrt(d)) / (2 * a)
+  x_2 = (-b - Math.sqrt(d)) / (2 * a)
+  print "Дискриминант = #{d}. x_1 = #{x_1}"
+  puts "#{d > 0 ? (' , x_2 = ' + x_2.to_s) : ' '}" 
 else
-	puts "Дискриминант = #{d}. Корней нет"
+  puts "Дискриминант = #{d}. Корней нет"
 end
--- Lesson_1/weight.rb
+++ Lesson_1/weight.rb
@@ -1,7 +1,7 @@
 #ideal weight
 
 print "Привет, хочешь узнать свой идельаный вес? Как тебя зовут? "
-name = gets.chomp.capitalize # странно, почему не капитализирует русский?
+name = gets.chomp.capitalize 
 
 print "Каков твой рост? "
 weight = gets.chomp.to_i
@@ -9,7 +9,7 @@ weight = gets.chomp.to_i
 ideal_weight = weight - 100
 
 if ideal_weight > 0
-	puts "#{name}, Ваш идеальный вес #{ideal_weight}"
+  puts "#{name}, Ваш идеальный вес #{ideal_weight}"
 else
-	puts "#{name}, Ваш вес уже оптимальный ;)"
+  puts "#{name}, Ваш вес уже оптимальный ;)"
 end
